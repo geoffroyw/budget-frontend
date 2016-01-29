8930870682011c562c2d672edff09365202278f5
Geoffroy Weyer <webweyer@gmail.com>
2016-01-29 17:23:29 +0100

    display model on index

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -4,7 +4,7 @@ export default DS.Model.extend({
   amount_cents: DS.attr('integer'),
   currency: DS.attr('string'),
   date: DS.attr('date'),
-  name: DS.attr('string'),
+  description: DS.attr('string'),
   created_at: DS.attr('date', {readOnly: true}),
   updated_at: DS.attr('date', {readOnly: true}),
   payment_mean: DS.belongsTo('payment-mean')
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    "use strict";
+    var a = Ember.A();
+    for (var i = 0; i < 10; i++) {
+      a.push(this.store.createRecord('transaction', {
+        date: new Date(),
+        description: 'foo bar baz',
+        amount_cents: 101343,
+        currency: "USD"
+      }));
+    }
+    return a;
+  }
 });
