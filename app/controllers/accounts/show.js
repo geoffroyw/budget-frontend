df3caa83173e3994bd3dbf1ce7c8db4bc3423e25
Geoffroy Weyer <webweyer@gmail.com>
2016-02-11 21:48:35 +0100

    sort transaction on account screen

---
+++ app/controllers/accounts/show.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  sortingKey: ['date'],
+  sortedTransactions: Ember.computed.sort('model.transactions', 'sortingKey'),
+
+
+  actions: {
+    sort(key) {
+      "use strict";
+      this.set('sortingKey', [key]);
+    }
+  }
+});
