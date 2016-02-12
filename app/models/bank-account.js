cc4035a7244ab9197e19b5c95ee782cc233eff9d
Geoffroy Weyer <webweyer@gmail.com>
2016-02-12 18:52:49 +0100

    display bank account balance

--- app/models/bank-account.js
+++ app/models/bank-account.js
@@ -3,5 +3,13 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   currency: DS.attr('string'),
-  transactions: DS.hasMany('transactions')
-});
+  transactions: DS.hasMany('transactions'),
+
+
+  transactionAmounts: Ember.computed.mapBy('transactions', 'amount_cents'),
+  balance: Ember.computed.sum('transactionAmounts'),
+
+  isBalanceNegative: Ember.computed.lt('balance', 0)
+
+})
+;
