c805e91e977cfd3e97c95e6ececdb126a89722fd
Geoffroy Weyer <webweyer@gmail.com>
2016-02-21 14:35:31 +0100

    modal to create bank account at first login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,16 +2,26 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   showTransactionForm: false,
+  currencyService: Ember.inject.service('currency'),
 
   transactionSorting: ['date'],
+
+  newAccount: {name: '', currency: ''},
+
+  transactions: Ember.computed.alias('model.transactions'),
+  bankAccounts: Ember.computed.alias('model.bankAccounts'),
+  paymentMeans: Ember.computed.alias('model.paymentMeans'),
+  categories: Ember.computed.alias('model.categories'),
+
+  hasBankAccounts: Ember.computed.notEmpty('bankAccounts'),
 
   sortedContent: Ember.computed.sort('model.transactions', 'transactionSorting'),
 
   actions: {
     /*newTransaction() {
-      "use strict";
-      this.set('newTransaction', this.store.createRecord('transaction', {date: new Date()}));
-    },*/
+     "use strict";
+     this.set('newTransaction', this.store.createRecord('transaction', {date: new Date()}));
+     },*/
 
     openTransactionFormModal() {
       "use strict";
@@ -37,6 +47,24 @@ export default Ember.Controller.extend({
       "use strict";
       this.set('selectedTransaction', transaction);
       this.set('showTransactionForm', true);
+    },
+
+    createAccount() {
+      "use strict";
+      let bankAccount = this.store.createRecord('BankAccount', {
+        name: this.get('newAccount.name'),
+        currency: this.get('newAccount.currency')
+      });
+
+      const flashMessages = Ember.get(this, 'flashMessages');
+
+      bankAccount.save().then(function () {
+        flashMessages.success('Bank Account successfully created!');
+      }).catch(function () {
+        flashMessages.danger('Error while saving the account. Please try again');
+        bankAccount.rollbackAttributes();
+      });
+
     }
   }
 });
